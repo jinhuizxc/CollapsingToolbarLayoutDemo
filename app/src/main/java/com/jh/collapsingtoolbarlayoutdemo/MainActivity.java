0f7d3193a8e7b9b54c73537def6b80a24086392c
jinhuizxc <1004260403@qq.com>
2018-09-19 20:40:12 +0800

    Initial commit

---
+++ app/src/main/java/com/jh/collapsingtoolbarlayoutdemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.jh.collapsingtoolbarlayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * https://www.jianshu.com/p/c3519b0f9266
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
